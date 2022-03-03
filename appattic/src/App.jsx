35db6d151fc8507d3de88281f5527daaf631d97a
rahul rai <rairahulr1@gmail.com>
2022-03-04 02:51:05 +0530

    added all project files

---
+++ appattic/src/App.jsx
@@ -0,0 +1,16 @@
+import Quote from "./Quote";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Quote />
+      <footer>
+        <a href="https://rairahulr1.github.io/appattic/dist/index.html">
+          Project Home
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
